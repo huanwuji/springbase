416a8e95427e8538e2ba50a68c8fa6708693599e
huanwuji <598452899@qq.com>
2012-09-02 08:45:39 +0800

    my project

---
+++ src/main/java/com/huanwuji/repository/ExampleRepository.java
@@ -0,0 +1,16 @@
+package com.huanwuji.repository;
+
+import org.hibernate.criterion.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created with IntelliJ IDEA.
+ * <p/>
+ * User: juyee
+ * Date: 12-8-3
+ * Time: 下午2:36
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ExampleRepository extends JpaRepository<Example, Long> {
+
+}
---
+++ src/main/java/com/huanwuji/web/ExampleController.java
@@ -0,0 +1,32 @@
+package com.huanwuji.web;
+
+import com.huanwuji.entity.bean.Example;
+import com.huanwuji.service.ExampleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * <p/>
+ * User: juyee
+ * Date: 12-8-3
+ * Time: 下午4:49
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class ExampleController {
+
+    @Autowired
+    ExampleService exampleService;
+
+    @RequestMapping
+    public String view(Model model) {
+        List<Example> examples = exampleService.getExamples();
+        model.addAttribute("beans", examples);
+        return "view";
+    }
+}
