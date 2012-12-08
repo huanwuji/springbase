c30c7873d46f77ff9a8a172f51d6ba6058175e57
huanwuji <huanwuji@github.com>
2012-12-08 19:22:19 +0800

    my project

--- src/test/java/com/huanwuji/utils/codeBatchCreate/FloatTest.java
+++ src/test/java/com/huanwuji/utils/codeBatchCreate/FloatTest.java
@@ -1,10 +1,8 @@
-package com.huanwuji.tools.codeBatchCreate;
-
-import org.apache.commons.lang3.StringUtils;
+package com.huanwuji.utils.codeBatchCreate;
 
 /**
  * Created with IntelliJ IDEA.
- *
+ * <p/>
  * User: huanwuji
  * Date: 12-9-2
  * Time: 下午12:31
@@ -13,7 +11,8 @@ import org.apache.commons.lang3.StringUtils;
 public class FloatTest {
 
     public static void main(String[] args) {
-//        System.out.println(" Float.MAX_VALUE = " + Long.MAX_VALUE);
+        System.out.println(" Float.MAX_VALUE = " + String.valueOf(Long.MAX_VALUE).length());
+        System.out.println(" Float.MAX_VALUE = " + String.valueOf(Integer.MAX_VALUE).length());
 
     }
 }
